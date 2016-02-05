f3bddc9bcd5fa29a777df398cf0a3c0723ab7a17
bastien1988 <bastienadamo@msn.com>
2016-02-05 10:01:40 +0100

    a

--- test.R
+++ test.R
@@ -2,7 +2,7 @@
 path <- "C:/kaggle/HomeDepot/"
 
 # Load Attributes of products
-attr      <- read.csv(paste(path,"attributes.csv", sep = ""))
+attr <- read.csv(paste(path,"attributes.csv", sep = ""))
 
 attr$product_uid <- as.factor(attr$product_uid)
 attr$name        <- as.factor(attr$name)
@@ -21,7 +21,7 @@ prod_desc$product_uid         <- as.factor(prod_desc$product_uid)
 prod_desc$product_description <- as.character(prod_desc$product_description)
 
 # Load Training data
-train     <- read.csv(paste(path,"train.csv", sep = ""))
+train <- read.csv(paste(path,"train.csv", sep = ""))
 
 train$product_title <- as.character(train$product_title)
 train$search_term   <- as.character(train$search_term)
@@ -40,6 +40,5 @@ search_terms <- unlist(strsplit(search_string, " "))
 # for(j in 1:length(search_terms))
   j <- 1
   grep(search_terms[j], name)
-
-
-assfsa
+  
+  
